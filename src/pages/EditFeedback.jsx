70ad6a23577d9566360c4c192a7a416b9e63f44a
mucahityildiz1 <mucahityyildiz@gmail.com>
2025-02-13 16:24:39 +0300

    new feedback page sayfasi kodlandi.

--- src/pages/EditFeedback.jsx
+++ src/pages/EditFeedback.jsx
@@ -1,10 +1,14 @@
-
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { DataContext } from "../App";
 
 export default function EditFeedback() {
+  const { feedbackId } = useParams();
 
+  const { data, setData } = useContext(DataContext);
   return (
     <>
-      <h1>edit</h1>
+     
     </>
-  )
+  );
 }
