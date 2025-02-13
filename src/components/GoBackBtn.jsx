85f1d79cd092a22449386079df8310ad98e80a60
Hacer Sude Çakır <sudecakir_62@hotmail.com>
2025-02-13 12:53:52 +0300

    feedbackDetails güncellendi

--- src/components/GoBackBtn.jsx
+++ src/components/GoBackBtn.jsx
@@ -1,7 +1,10 @@
 export default function GoBackBtn(){
   return(
     <>
-      
+      <div className="go-back-btn">
+        <img src="/images/arrow-left.svg"/>
+        <p>Go Back</p>
+      </div>
     </>
   )
 }
